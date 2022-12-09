baf6db8e2a59d780e06699e4e78cc6c60a0f393d
wyhong3103 <wyhong3103@gmail.com>
2022-12-09 11:51:04 +0800

    Finish core structure of card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
-
-const Card = () => {
-    
+const Card = (props) => {
+    <div className="card" onClick={props.callback}>
+        <h3 className="card-number">props.number</h3>
+    </div>
 };
 
 export default Card;
