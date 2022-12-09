1487c7280ac1860b3311757ac8b829c10008d96d
wyhong3103 <wyhong3103@gmail.com>
2022-12-09 11:27:23 +0800

    Add winning and losing function

--- src/App.js
+++ src/App.js
@@ -7,6 +7,27 @@ const App = () => {
     const [best, setBest] = useState(0);
     const [currentSize, setCurrentSize] = useState(2);
     const [used, setUsed] = useState(new Set());
+    const [currentCards, setCurrentCards] = useState([1, 2]);
+
+    const win = () => {
+        let curScore = 0;
+        setCurrent(prev => {
+            curScore = prev;
+            return prev+1;
+        });
+
+        if (curScore > best){
+            setBest(curScore);
+        }
+
+        //reset function
+    }
+
+    const lose = () => {
+        setCurrent(0);
+
+        //reset function
+    }
 
     return(
         <div>
