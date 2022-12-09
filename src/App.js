95fa73168583fad95465573462a09804a12bfde6
wyhong3103 <wyhong3103@gmail.com>
2022-12-09 11:32:14 +0800

    Update win/lose func and change used to a non state variable

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Game from "./components/Game";
 import Header from "./components/Header";
 import Scoreboard from "./components/Scoreboard";
 
@@ -6,8 +7,10 @@ const App = () => {
     const [current, setCurrent] = useState(0);
     const [best, setBest] = useState(0);
     const [currentSize, setCurrentSize] = useState(2);
-    const [used, setUsed] = useState(new Set());
     const [currentCards, setCurrentCards] = useState([1, 2]);
+
+    // A set to store all selected numbers
+    const used = new Set();
 
     const win = () => {
         let curScore = 0;
@@ -20,11 +23,17 @@ const App = () => {
             setBest(curScore);
         }
 
+        if (curScore >= currentSize){
+            setCurrentSize(prev => prev * 2);
+        }
+
         //reset function
     }
 
     const lose = () => {
         setCurrent(0);
+        setCurrentSize(2);
+        used.clear();
 
         //reset function
     }
@@ -33,6 +42,7 @@ const App = () => {
         <div>
             <Header></Header>
             <Scoreboard current={current} best={best}></Scoreboard>
+            <Game cards={currentCards}></Game>
         </div>
     )
 };
