ab874e8d579ddce95baeb2d33c6285c598bf7036
wyhong3103 <wyhong3103@gmail.com>
2022-12-09 11:58:29 +0800

    Finish up core structure of game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,23 @@
-import { useEffect } from "react";
+import Card from "./Card";
 
 const Game = (props) => {
+    return(
+        <div className="game">
+            {
+                props.cards.map(
+                    (item, index) => {
+                        return(
+                            <Card 
+                                key={index} 
+                                number={item[0]} 
+                                callback={item[1] === 1 ? props.lose : props.win}
+                            ></Card>
+                        )
+                    }
+                )
+            }
+        </div>
+    )
 };
 
 export default Game;
