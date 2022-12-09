b88dec050e2372222a8bf0bdcff2936719e1d357
wyhong3103 <wyhong3103@gmail.com>
2022-12-09 11:18:41 +0800

    Add states to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import { useState } from "react";
+import Header from "./components/Header";
 
 const App = () => {
- 
+    const [current, setCurrent] = useState(0);
+    const [best, setBest] = useState(0);
+    const [currentSize, setCurrentSize] = useState(2);
+    const [used, setUsed] = useState(new Set());
+
+    return(
+        <div>
+        </div>
+    )
 };
 
 export default App;
