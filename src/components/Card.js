a390075526ab5007606389139f6a83d020c2fcb3
wyhong3103 <wyhong3103@gmail.com>
2022-12-09 12:01:57 +0800

    Fix card.js not rendering

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,9 @@
 const Card = (props) => {
-    <div className="card" onClick={props.callback}>
-        <h3 className="card-number">props.number</h3>
-    </div>
+    return(
+        <div className="card" onClick={props.callback}>
+            <h3 className="card-number">{props.number}</h3>
+        </div>
+    )
 };
 
 export default Card;
